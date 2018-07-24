14bc1501c64c4cf418841b165387f7a1c018bee4
vivekpan27 <vivek.panicker@innoraft.com>
2018-07-24 18:37:25 +0530

    Temp commit.

--- index.js
+++ index.js
@@ -9,112 +9,40 @@ const s3 = new aws.S3({
 
 exports.handler = (event, context, callback) => {
 
-  /*
-  Function is called for old csv file.
-  */
-  function csvToJsonIdRowMapping(file) {
-    let s3Stream = s3.getObject({ Bucket: config.bucket_name, Key: file }).createReadStream();
-    let jsonBreakDown = [];
-    let count = 0;
-    let id = 0;
-    let id_column = -1;
-    let id_column_value = 'id';
-    let tempjsonObj={}, jsonObj = [];
-    let rowNumber = 0;
-    let params = {
-      Body: '',
-      Bucket: config.bucket_name,
-      Key: '',
-    }
+  let params = {
+    Bucket: config.bucket_name,
+    Prefix: 'json/',
+  };
 
-    let csvStream = csv()
-      .on('header', (header) => {
-        id_column = header.indexOf(id_column_value);
-      })
-      .on('data', (data) => {
-        const jsonStr = data.toString('utf8');
-        tempjsonObj = JSON.parse(jsonStr);
-        jsonObj.push({'id' : tempjsonObj['id'], 'row' : rowNumber});
+  let jsonFolderContents = {};
+  let validKeys = [];
+  let oldCsvMapping = '', newCsvMapping = '';
 
-        rowNumber++;
-      })
-      .on('done', (error)=> {
-        params.Body = JSON.stringify(jsonObj, null, 2);
-        params.Key = 'json/old_csv_mapping.json';
-        s3.putObject(params, function(err, data) {
-          if (err) throw err;
-        });
-      });
+  s3.listObjectsV2(params, function(err, data) {
+    if (err) throw err;
+    jsonFolderContents = data.Contents;
+    // console.log(jsonFolderContents);
+    // console.log(jsonFolderContents.length);
+    for (var i = jsonFolderContents.length - 1; i >= 0; i--) {
 
-    s3Stream.pipe(csvStream);
-  }
-
-  /*
-  Function is called for new csv file.
-  */
-  function csvToJsonIdRowMappingAndCsvSplit(file) {
-    let s3Stream = s3.getObject({ Bucket: config.bucket_name, Key: file }).createReadStream();
-    let jsonBreakDown = [];
-    let count = 0;
-    let id = 0;
-    let id_column = -1;
-    let id_column_value = 'id';
-    let tempjsonObj={}, jsonObj = [];
-    let rowNumber = 0;
-    let params = {
-      Body: '',
-      Bucket: config.bucket_name,
-      Key: '',
-    }
-
-    let csvStream = csv()
-      .on('header', (header) => {
-        id_column = header.indexOf(id_column_value);
-      })
-      .on('data', (data) => {
-        const jsonStr = data.toString('utf8');
-
-        tempjsonObj = JSON.parse(jsonStr);
-        jsonObj.push({'id' : tempjsonObj['id'], 'row' : rowNumber});
-
-        jsonBreakDown.push(jsonStr);
-        if (jsonBreakDown.length === 10000) {
-          params.Body = JSON.stringify(jsonBreakDown, null, 2);
-          params.Key = 'json/file' + count + '.json';
-          s3.putObject(params, function(err, data) {
-            if (err) throw err;
-          });
-          jsonBreakDown = [];
-          count++;
+      let key = jsonFolderContents[i].Key;
+      let split_key = key.split('/');
+      if (!(split_key[1] == '')) {
+        if (split_key[1].search(/file/) == 0) {
+          validKeys.push(key);
         }
-
-        rowNumber++;
-      })
-      .on('done', (error)=> {
-        if (jsonBreakDown.length > 0) {
-          params.Body = JSON.stringify(jsonBreakDown, null, 2);
-          params.Key = 'json/file' + count + '.json';
-          s3.putObject(params, function(err, data) {
-            if (err) throw err;
-          });
+        else if (split_key[1].search(/new/) == 0) {
+          newCsvMapping = key;
         }
-
-
-        params.Body = JSON.stringify(jsonObj, null, 2);
-        params.Key = 'json/new_csv_mapping.json';
-        s3.putObject(params, function(err, data) {
-          if (err) throw err;
-        });
-      });
-
-    s3Stream.pipe(csvStream);
-  }
-  /* Function definitions end */
-
-  let newCsvFile = event.Records[0].s3.object.key;
-  let oldCsvFile = 'template-sap-user-data/b.csv';
-  csvToJsonIdRowMappingAndCsvSplit(newCsvFile);
-  csvToJsonIdRowMapping(oldCsvFile);
+        else if (split_key[1].search(/old/) == 0) {
+          oldCsvMapping = key;
+        }
+      }
+    }
+    console.log('Valid ', validKeys);
+    console.log('New ', newCsvMapping);
+    console.log('Old' , oldCsvMapping);
+  });
 
   callback(null, 'Hello from Lambda');
 };
