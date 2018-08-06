2b22f9d020710466deffb90376ff2aa23fcd0f24
vivekpan27 <vivek.panicker@innoraft.com>
2018-08-07 01:02:41 +0530

    index.js facing permission problem. Rest alright.

--- index.js
+++ index.js
@@ -1,184 +1,150 @@
+console.log("Beginnning of lambda 1");
 const csv=require('csvtojson'),
 aws = require('aws-sdk'),
 config = require('./config'),
-hash = require('object-hash'),
+mysql = require('mysql'),
 async = require('async'),
-JsonToCsvParser = require('json2csv').Parser,
-mysql = require('mysql');
-// fs = require('fs'),
-
-// From Here
-
-const s3 = new aws.S3({
+s3 = new aws.S3({
   "accessKeyId" : config.access_key_id,
   "secretAccessKey": config.secret_access_key,
+}),
+lambda = new aws.Lambda({
+  region: 'us-west-2' //change to your region
 });
-
-// const con = mysql.createConnection({
-//   host: config.mysql.host,
-//   user: config.mysql.user,
-//   password: config.mysql.password,
-//   database: config.mysql.database,
-// });
 
 exports.handler = (event, context, callback) => {
 
-  let params = {
-    Bucket: config.bucket_name,
-    Prefix: 'json/',
-  };
-
-  let jsonFolderContents = {};
-  let splitJsonFileKeys = [];
-  let oldCsvMappingKey = '', newCsvMappingKey = '';
-  let newInsertedData = [];
-  let newUpdatedData = [];
-  let oldCsvJson = '';
-  let oldCsvMappingJson = '';
-
-  s3.listObjectsV2(params, function(err, data) {
-    if (err) throw err;
-    jsonFolderContents = data.Contents;
-
-    for (var i = jsonFolderContents.length - 1; i >= 0; i--) {
-
-      let key = jsonFolderContents[i].Key;
-      let split_key = key.split('/');
-      if (!(split_key[1] == '')) {
-        if (split_key[1].search(/file/) == 0) {
-          splitJsonFileKeys.push(key);
-        }
-        else if (split_key[1].search(/new/) == 0) {
-          newCsvMappingKey = key;
-        }
-        else if (split_key[1].search(/old/) == 0) {
-          oldCsvMappingKey = key;
-        }
-      }
+console.log('In exports handler of lambda 1');
+  /*
+  Function is called for old csv file.
+  */
+  function csvToJsonIdRowMapping(file) {
+    let s3Stream = s3.getObject({ Bucket: config.bucket_name, Key: file }).createReadStream();
+    let jsonBreakDown = [];
+    let count = 0;
+    let id = 0;
+    let id_column = -1;
+    let id_column_value = 'id';
+    let tempjsonObj={}, jsonObj = [];
+    let rowNumber = 0;
+    let params = {
+      Body: '',
+      Bucket: config.bucket_name,
+      Key: '',
     }
 
-    console.log('Valid ', splitJsonFileKeys);
-    console.log('New ', newCsvMappingKey);
-    console.log('Old' , oldCsvMappingKey);
+    let csvStream = csv()
+      .on('header', (header) => {
+        id_column = header.indexOf(id_column_value);
+      })
+      .on('data', (data) => {
+        const jsonStr = data.toString('utf8');
+        tempjsonObj = JSON.parse(jsonStr);
+        jsonObj[tempjsonObj['id']] = rowNumber;
 
-    let performOps = function(file, done) {
-      console.log('In performOps for file', file);
-      s3.getObject({ Bucket: config.bucket_name, Key: file }, function(err, data) {
-        // Get the split json file values.
-        jsonFileData = data.Body.toString();
-        jsonFileData = JSON.parse(jsonFileData);
-        console.log('oldcsvmappingjson ', oldCsvMappingJson[1]);
-        // Loop through all the elements in the new file.
-        for (var i = jsonFileData.length - 1; i >= 0; i--) {
-          // Check if 'id' of new_csv json file exists in the mapping file of old csv.
-          if(jsonFileData[i].id in oldCsvMappingJson) {
-            // Get the row number of that id in the old csv file.
-            oldCsvRowVal = oldCsvMappingJson[jsonFileData[i].id];
-            // Get the row details in the old file.
-            oldCsvRow = oldCsvJson[oldCsvRowVal];
-            // Get the hash value of the row in the old file.
-            oldCsvRowHash = hash(oldCsvRow);
-            // Get the hash value of the row in the new file.
-            newCsvRowHash = hash(jsonFileData[i]);
-            // If hashes not equal, that means data is updated.
-            if (oldCsvRowHash != newCsvRowHash) {
-              oldCsvJson[oldCsvRowVal] = jsonFileData[i];
-              // newUpdatedData.push(jsonFileData[i]);
-              console.log('Updated!');
-            }
-          }
-          else {
-            // If id is not present in old csv file, that means insertion has occured.
-            newInsertedData.push(jsonFileData[i]);
-            console.log("Inserted");
-          }
-        }
-
-        console.log('Returning from performops for file ', file);
-
-        return done(null, oldCsvJson);
-      });
-    }
-
-    async.series([
-      function(callback) {
-        s3.getObject({ Bucket: config.bucket_name, Key: 'template-sap-user-data/b.csv' }, function(err, data) {
-           console.log('Old Csv data object fetched');
-           csv()
-            .fromString(data.Body.toString())
-            .then((json) => {
-              oldCsvJson = json;
-              console.log('Old Csv Json loaded');
-              callback(null);
-            });
-        });
-      },
-      function(callback) {
-        s3.getObject({ Bucket: config.bucket_name, Key: 'json/old_csv_mapping.json' }, function(err, data){
-          console.log('Old csv mapping file fetched');
-          oldCsvMappingJson = JSON.parse(data.Body.toString());
-          // callback(null, oldCsvJson, oldCsvMappingJson);
-          callback(null);
-        });
-      },
-      function(callback) {
-        async.mapLimit(splitJsonFileKeys, 5, performOps, function(err, updatedExistingJsonData) {
+        rowNumber++;
+      })
+      .on('done', (error)=> {
+        params.Body = JSON.stringify(jsonObj, null, 2);
+        params.Key = 'json/old_csv_mapping.json';
+        s3.putObject(params, function(err, data) {
           if (err) throw err;
-          console.log('Length ', updatedExistingJsonData.length);
-          console.log(Object.keys(updatedExistingJsonData));
-          for (var i = 1; i <= updatedExistingJsonData.length - 1;  i++) {
-            console.log('Length of  updatedExistingJsonData[i]', updatedExistingJsonData[i].length);
-            updatedExistingJsonData[0].concat(updatedExistingJsonData[i]);
-          }
-          // console.log('new inserted data', newInsertedData);
-          // console.log('new updated data', newUpdatedData);
-          let dbData = {};
-          dbData.insert = newInsertedData;
-          dbData.update = updatedExistingJsonData[0];
-          dbData.complete = dbData.update.concat(dbData.insert);
-
-
-          var db_insert_values_array = Object.keys(dbData.complete).map(function(key) {
-            return [dbData.complete[key].id, dbData.complete[key].sap, dbData.complete[key].email];
-          });
-          console.log("Befor connect");
-
-          //Mysql insert
-          var con = mysql.createConnection({
-            host: config.mysql.host,
-            user: config.mysql.user,
-            password: config.mysql.password,
-            database: config.mysql.database,
-          });
-
-          con.connect(function(err) {
-            if(err) throw err;
-            var table = config.mysql.table;
-
-            // First delete existing records from table.
-            console.log("In delete function");
-            var sql = "DELETE FROM " + table;
-            con.query(sql, function(err, result) {
-              if (err) throw error;
-              console.log(table + ' table truncated!');
-              console.log("Number of records deleted: " + result.affectedRows);
-
-              // Then insert complete data into table.
-              console.log("In connect function");
-              var sql = "INSERT INTO " + table + " (id, sap, email) VALUES ?";
-
-              con.query(sql, [db_insert_values_array], function(err, result){
-                if (err) throw err;
-                console.log('Insertion complete : ' + result.affectedRows);
-              });
-              con.end();
-            });
-          });
         });
-        callback(null);
-      }
-    ]);
+      });
 
+    s3Stream.pipe(csvStream);
+  }
+
+  /*
+  Function is called for new csv file.
+  */
+  function csvToJsonIdRowMappingAndCsvSplit(file) {
+    let s3Stream = s3.getObject({ Bucket: config.bucket_name, Key: file }).createReadStream();
+    let jsonBreakDown = [];
+    let count = 0;
+    let id = 0;
+    let id_column = -1;
+    let id_column_value = 'id';
+    let tempjsonObj={}, jsonObj = [];
+    let rowNumber = 0;
+    let params = {
+      Body: '',
+      Bucket: config.bucket_name,
+      Key: '',
+    }
+
+    let csvStream = csv()
+      .on('header', (header) => {
+        id_column = header.indexOf(id_column_value);
+      })
+      .on('data', (data) => {
+        const jsonStr = data.toString('utf8');
+
+        tempjsonObj = JSON.parse(jsonStr);
+        jsonObj.push({'id' : tempjsonObj['id'], 'row' : rowNumber});
+
+        jsonBreakDown.push(jsonStr);
+        if (jsonBreakDown.length === 10000) {
+          params.Body = '[' + jsonBreakDown + ']';
+          params.Key = 'json/file' + count + '.json';
+          s3.putObject(params, function(err, data) {
+            if (err) throw err;
+          });
+          jsonBreakDown = [];
+          count++;
+        }
+
+        rowNumber++;
+      })
+      .on('done', (error)=> {
+        if (jsonBreakDown.length > 0) {
+          params.Body = '[' + jsonBreakDown + ']';
+          params.Key = 'json/file' + count + '.json';
+          s3.putObject(params, function(err, data) {
+            if (err) throw err;
+          });
+        }
+
+
+        params.Body = JSON.stringify(jsonObj, null, 2);
+        params.Key = 'json/new_csv_mapping.json';
+        s3.putObject(params, function(err, data) {
+          if (err) throw err;
+        });
+      });
+
+    s3Stream.pipe(csvStream);
+  }
+  /* Function definitions end */
+
+  let newCsvFile = event.Records[0].s3.object.key;
+  let oldCsvFile = 'template-sap-user-data/b.csv';
+
+  async.parallel([
+    function(callback) {
+      csvToJsonIdRowMappingAndCsvSplit(newCsvFile);
+      console.log('New csv file mapping created.');
+      callback();
+    },
+    function(callback) {
+      csvToJsonIdRowMapping(oldCsvFile);
+      console.log('Old csv file mapping created.');
+      callback();
+    },
+  ],
+  function(err, results) {
+    console.log('In callback function for async parallel.');
+    var params = {
+        FunctionName: 'hcl-alumni-data-migrate-2',
+        InvocationType: 'Event',
+        Payload: '',
+        Qualifier: context.functionVersion,
+    };
+    lambda.invoke(params, function(err, result) {
+        if (err) throw err;
+        console.log('Called 2nd lambda function successful');
+        callback(null, result.message);
+    });
   });
 
   callback(null, 'Hello from Lambda');
